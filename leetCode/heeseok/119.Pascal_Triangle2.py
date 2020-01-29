686d228b6c3a191981b03692f6b6912165b6f02c
“nhs0912” <nhs0912@nate.com>
2020-01-29 12:10:22 +0900

    118,119 문제 추가

---
+++ leetCode/heeseok/119.Pascal_Triangle2.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        outList = self.generate(rowIndex)
+        return outList[-1]
+
+    def generate(self, numRows: int) -> List[List[int]]:
+        arrayList = []
+        numberList = []
+        if numRows > 0:
+            numberList.append([1])
+        if numRows > 1:
+            numberList.append([1, 1])
+        if numRows > 2:
+
+            for i in range(3, numRows + 1):
+                numList2 = []
+                numList2.append(1)
+                size = len(numberList[i - 2])
+                for j in range(0, size - 1):
+                    sum = numberList[i - 2][j] + numberList[i - 2][j + 1]
+                    numList2.append(sum)
+                numList2.append(1)
+                numberList.append(numList2)
+
+        return numberList
