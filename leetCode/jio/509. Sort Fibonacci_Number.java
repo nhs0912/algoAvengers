d113698d7cc1535cc3577e29b0ae71a5175cbf57
kimjio222 <32680685+kimjio222@users.noreply.github.com>
2020-06-17 12:46:53 +0900

    Update 509. Sort Fibonacci_Number.java

--- leetCode/jio/509. Sort Fibonacci_Number.java
+++ leetCode/jio/509. Sort Fibonacci_Number.java
@@ -15,3 +15,15 @@ class Solution {
         return nums[N];
     }
 }
+
+// 재귀함수이용
+class Solution {
+    public int fib(int N) {
+        
+        if(N > 1){
+            return fib(N-1) + fib(N-2);            
+        }else{            
+            return N;            
+        }        
+    }
+}
