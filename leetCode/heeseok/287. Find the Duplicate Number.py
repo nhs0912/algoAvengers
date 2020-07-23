fd36f236ffc6ec7ba346512f76b8b19d1f2672a4
HeeSeok Noh <nhs0912@nate.com>
2020-07-23 12:38:45 +0900

    Update 287. Find the Duplicate Number.py

--- leetCode/heeseok/287. Find the Duplicate Number.py
+++ leetCode/heeseok/287. Find the Duplicate Number.py
@@ -1,12 +1,9 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        nums = sorted(nums)
         numsSize = len(nums)
-        baseSum = ((numsSize - 1) * numsSize) / 2
-        arrSum = 0
-        if numsSize>1 and nums[0] == nums[1]:
+        if numsSize == 1:
             return nums[0]
-            
-        for i in range(0, numsSize):
-            arrSum = arrSum + nums[i]
-
-        return int(arrSum - baseSum)
+        for i in range(1,numsSize):
+            if nums[i-1]==nums[i]:
+                return nums[i]
