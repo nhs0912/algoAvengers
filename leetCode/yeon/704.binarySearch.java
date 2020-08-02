ab1b59875a0f91db7a621d7c6c4fb47bd1530087
yeon127 <lovegroup@naver.com>
2020-08-02 15:46:33 +0900

    Create 704.binarySearch.java

---
+++ leetCode/yeon/704.binarySearch.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int search(int[] nums, int target) {
+            
+        int point = nums.length / 2;
+        
+        return binarySearch(nums , target, point);
+        
+    }
+    
+    public int binarySearch(int[] nums, int target , int point ){
+        
+        if(nums.length > point){ 
+            if(target == nums[point]){ 
+             // 동일하면 index 반환 
+            
+               return point;
+           }else if(target > nums[point]){
+            
+            if(point == point + ((nums.length-point)/2)){
+                return -1;
+            }
+        
+             point = point + ((nums.length-point)/2);
+            
+             return binarySearch(nums, target, point);
+                
+            }else{
+              if(point == (point/2)){
+                  return -1;
+              }
+              point = point / 2;
+            
+              return binarySearch(nums, target, point);
+            }
+        }
+        return -1;
+    }
+}
